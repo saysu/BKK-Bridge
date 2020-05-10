fcaf87ee0eb0e888fb718473063ff429cd5a2416
saysu <suzukisayaka0109@gmail.com>
2020-05-10 12:46:23 +0700

    Add Procfile + varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +23,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+
+    public function boot(UrlGenerator $url)
     {
-        Schema::defaultStringLength(191);
+    $url->forceScheme('https');
+    Schema::defaultStringLength(191);
     }
 }
